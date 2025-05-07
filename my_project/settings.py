8428f420baf25cf10070ad41120d17f5466f432c
NaMlex <alexn.alexn@gmail.com>
2025-05-07 13:29:29 +0200

    django app

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sbwd!gp#gq2+1evbfl4-g155ezwo$s$!+i3u7^gc!apnjh_kwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
